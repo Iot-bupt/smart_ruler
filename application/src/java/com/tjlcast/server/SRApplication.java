8e5e6654befb6ce2c7ebb36e234aaffeb288bf08
tangjialiang <phx108@sina.com>
2018-04-13 16:17:34 +0800

    1.0.0 version

---
+++ application/src/java/com/tjlcast/server/SRApplication.java
@@ -0,0 +1,19 @@
+package com.tjlcast.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by tangjialiang on 2017/12/19.
+ *
+ */
+
+//@ComponentScan({"com.tjlcast.transport.netty"})
+//@ComponentScan({"com.tjlcast.transport.akka"})
+@SpringBootApplication
+public class SRApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SRApplication.class, args);
+    }
+}
