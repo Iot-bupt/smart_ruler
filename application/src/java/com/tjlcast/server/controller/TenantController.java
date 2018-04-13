50e873e45c7181b538d933fa91771fcc313e5c86
tangjialiang <phx108@sina.com>
2018-04-13 17:29:25 +0800

    1.0.0 version

---
+++ application/src/java/com/tjlcast/server/controller/TenantController.java
@@ -0,0 +1,11 @@
+package com.tjlcast.server.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by tangjialiang on 2018/4/13.
+ */
+
+@RestController
+public class TenantController extends ActorAwareController {
+}
