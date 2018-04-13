90ce0988839b1dc023cb9da8fffb4b958b537c1e
youzi <liyou@bupt.edu.cn>
2018-04-13 17:31:38 +0800

    liyou:4月13号17点30分更新RuleActor

---
+++ application/src/java/com/tjlcast/server/actors/device/RuleActor.java
@@ -0,0 +1,70 @@
+package com.tjlcast.server.actors.device;
+
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import com.tjlcast.common.message.device.DeviceRecognitionMsg;
+import com.tjlcast.common.message.device.DeviceShadowMsg;
+import com.tjlcast.common.message.device.ToDeviceActorNotificationMsg;
+import com.tjlcast.server.actors.ActorSystemContext;
+import com.tjlcast.server.actors.service.ContextAwareActor;
+import com.tjlcast.server.actors.service.ContextBasedCreator;
+
+import java.util.UUID;
+
+/**
+ * Created by tangjialiang on 2017/12/8.
+ */
+public class RuleActor extends ContextAwareActor {
+
+    private final LoggingAdapter logger = Logging.getLogger(getContext().system(), this) ;
+
+    private final UUID tenantId ;
+    private final UUID ruleId ;
+    private final RuleActorMessageProcessor processor;
+
+    private RuleActor(ActorSystemContext context, UUID tenantId, UUID ruleId) {
+        super(context) ;
+        this.tenantId = tenantId ;
+        this.ruleId = ruleId ;
+        this.processor = new RuleActorMessageProcessor(systemContext, logger, ruleId);
+    }
+
+    @Override
+    public void onReceive(Object message) throws Exception {
+        if(message instanceof DeviceRecognitionMsg){
+            processor.process((DeviceRecognitionMsg)message);
+        } else if (message instanceof ToDeviceActorNotificationMsg) {
+//            if (msg instanceof DeviceAttributesEventNotificationMsg) {
+//                processor.processAttributesUpdate(context(), (DeviceAttributesEventNotificationMsg) msg);
+//            } else if (msg instanceof ToDeviceRpcRequestPluginMsg) {
+//                processor.processRpcRequest(context(), (ToDeviceRpcRequestPluginMsg) msg);
+//            } else if (msg instanceof DeviceCredentialsUpdateNotificationMsg){
+//                processor.processCredentialsUpdate();
+//            } else if (msg instanceof DeviceNameOrTypeUpdateMsg){
+//                processor.processNameOrTypeUpdate((DeviceNameOrTypeUpdateMsg) msg);
+//                //TODO modified by cc
+//            }
+            if(message instanceof DeviceShadowMsg){
+                processor.processDeviceShadowMsg((DeviceShadowMsg)message);
+            }
+        }
+    }
+
+    public static class ActorCreator extends ContextBasedCreator<RuleActor> {
+        private static final long serialVersionUID = 1L ;
+
+        private final UUID tenantId ;
+        private final UUID ruleId ;
+
+        public ActorCreator(ActorSystemContext context, UUID tenantId, UUID ruleId) {
+            super(context) ;
+            this.tenantId = tenantId ;
+            this.ruleId = ruleId ;
+        }
+
+        @Override
+        public RuleActor create() throws Exception {
+            return new RuleActor(context, tenantId, ruleId) ;
+        }
+    }
+}
---
+++ application/src/java/com/tjlcast/server/actors/device/RuleActorMessageProcessor.java
@@ -0,0 +1,128 @@
+package com.tjlcast.server.actors.device;
+
+import akka.event.LoggingAdapter;
+import com.google.gson.JsonObject;
+import com.tjlcast.common.data.DeviceShadow;
+import com.tjlcast.common.message.device.DeviceRecognitionMsg;
+import com.tjlcast.common.message.device.DeviceShadowMsg;
+import com.tjlcast.server.actors.ActorSystemContext;
+import com.tjlcast.server.actors.shared.AbstractContextAwareMsgProcessor;
+import com.tjlcast.server.utils.http.demo.OkHttpUtil;
+import scala.concurrent.duration.Duration;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by tangjialiang on 2017/12/12.
+ */
+public class RuleActorMessageProcessor extends AbstractContextAwareMsgProcessor {
+
+    private final UUID ruleId;
+    private final Map<UUID, UUID> sessions;
+    private final Map<UUID, UUID> attributeSubscriptions;
+    private final Map<UUID, UUID> rpcSubscriptions;
+
+   private DeviceShadow deviceShadow;
+
+    public RuleActorMessageProcessor(ActorSystemContext systemContext, LoggingAdapter logger, UUID ruleId) {
+        super(systemContext, logger);
+        this.ruleId = ruleId;
+        this.sessions = new HashMap<>();
+        this.attributeSubscriptions = new HashMap<>();
+        this.rpcSubscriptions = new HashMap<>();
+
+        initAttributes();
+    }
+
+    private void initAttributes() {
+//        Device device = systemContext.getDeviceService().findDeviceById(deviceId);
+//        this.deviceName = device.getName();
+//        this.deviceType = device.getType();
+//        this.deviceAttributes = new DeviceAttributes(fetchAttributes(DataConstants.CLIENT_SCOPE),
+//                fetchAttributes(DataConstants.SERVER_SCOPE), fetchAttributes(DataConstants.SHARED_SCOPE));
+        // 初始化设备影子
+        //TODO 初始化设备影子并开启心跳
+        initDeviceShadow();
+        systemContext.getScheduler().schedule(Duration.create(2000, TimeUnit.MILLISECONDS),
+                Duration.create(2000, TimeUnit.MILLISECONDS), new Runnable() {
+                    @Override
+                    public void run() {
+                        //TODO 发送心跳
+                    }
+                },systemContext.getActorSystem().dispatcher());
+
+    }
+
+
+    private void initDeviceShadow(){
+//        Device device = systemContext.getDeviceService().findDeviceById(deviceId);
+//        String manufacture = device.getManufacture();
+//        String deviceType = device.getDeviceType();
+//        String model = device.getModel();
+//        if(StringUtil.checkNotNull(manufacture,deviceType,model)){
+//            deviceShadow = HttpUtil.getDeviceShadowDoc(manufacture,deviceType,model);
+//            deviceShadow.addProperty("deviceId",device.getId().toString());
+//            //TODO send to service midware
+//        }
+    };
+
+//    private void refreshAttributes(DeviceAttributesEventNotificationMsg msg) {
+//        if (msg.isDeleted()) {
+//            msg.getDeletedKeys().forEach(key -> deviceAttributes.remove(key));
+//        } else {
+//            deviceAttributes.update(msg.getScope(), msg.getValues());
+//        }
+//    }
+
+    public void process(DeviceRecognitionMsg msg){
+
+//        Device device = systemContext.getDeviceService().findDeviceById(deviceId);
+//        String manufacture = msg.getManufacture();
+//        String deviceType = msg.getDeviceType();
+//        String model = msg.getModel();
+//        if(StringUtil.checkNotNull(manufacture,deviceType,model)){
+//            deviceShadow = HttpUtil.getDeviceShadowDoc(manufacture,deviceType,model);
+//            deviceShadow.addProperty("deviceId",device.getId().toString());
+//            //TODO send to service midware
+//        }
+    }
+
+    public void processDeviceShadowMsg(DeviceShadowMsg msg){
+        //TODO  deiceactor中处理数据http请求
+        JsonObject payLoad = msg.getPayLoad();
+        String methodName = null ;
+        if(methodName==null){
+            // to www.baidu.com
+            String response = "bad response" ;
+            try {
+                // first try
+                response = OkHttpUtil.getStringFromServer("http://www.baidu.com");
+            } catch (IOException e) {
+                // second try
+                try {
+                    response = OkHttpUtil.getStringFromServer("http://www.baidu.com");
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            msg.setResult(response);
+        }else if(methodName.equals("get")){
+            msg.setResult(deviceShadow.getPayload().toString());
+        }else if(methodName.equals("updateAttribute")){
+            JsonObject attribute = payLoad.get("attribute").getAsJsonObject();
+            String attributeName = attribute.get("attributeName").getAsString();
+            String attributeValue = attribute.get("attributeValue").getAsString();
+//            KvEntry entry = new StringDataEntry(attributeName,attributeValue);
+//            AttributeKvEntry attr = new BaseAttributeKvEntry(entry,System.currentTimeMillis());
+//            List<AttributeKvEntry> ls = new ArrayList<AttributeKvEntry>();
+//            ls.add(attr);
+//            systemContext.getAttributesService().save(msg.getDeviceId(),"SERVER_SCOPE",ls);
+            deviceShadow.updateAttribute(attribute.get("attributeName").getAsString(),attribute);
+            msg.setResult(deviceShadow.getPayload().toString());
+        }else{
+            msg.setResult("Unrecognized methodName");
+        }
+    }
+}
