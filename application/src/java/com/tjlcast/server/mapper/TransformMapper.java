7f1ce8a6b9fbe68d5078e191efef81b9b96a9cbb
youzi <liyou@bupt.edu.cn>
2018-05-06 17:12:04 +0800

    liyou:并能够删除数据

--- application/src/java/com/tjlcast/server/mapper/TransformMapper.java
+++ application/src/java/com/tjlcast/server/mapper/TransformMapper.java
@@ -14,19 +14,19 @@ public interface TransformMapper{
     @Select("select * from t_transform")
     List<Transform> findAll();
 
-    @Select("select * from transform where id = #{id}")
-    Transform findById(Integer id) ;
+    @Select("select * from transform where transformId = #{transformId}")
+    Transform findById(Integer transformId) ;
 
-    @Select("select transform.id, transform.name, transform.url, transform.method from transform transform left join t_rule rule on transform.id = rule.transformId where rule.id = #{id}")
-    Transform findByRuleId(Integer id) ;
+    @Select("select transform.transformId, transform.name, transform.url, transform.method from transform transform left join rule rule on transform.transformId = rule.transformId where rule.ruleId = #{ruleId}")
+    Transform findByRuleId(Integer ruleId) ;
 
     @Delete("delete from transform")
     void deleteAll();
 
-    @Delete("delete from transform where id = #{id}")
-    void deleteById(Integer id);
+    @Delete("delete from transform where transformId = #{transformId}")
+    void deleteById(Integer transformId);
 
-    @Insert("insert into transform(name,url,method) VALUES (#{name}, #{url}, #{transformId})")
+    @Insert("insert into transform(name,url,method) VALUES (#{name}, #{url}, #{method})")
     @Options(useGeneratedKeys = true, keyProperty = "transformId", keyColumn = "transformId")
     int save(Transform transform);
 
