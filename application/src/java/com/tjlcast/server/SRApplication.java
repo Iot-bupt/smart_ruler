0f93055dad544a5828f8a8d1bca0b8d5111cbcae
czx <czxstc200@bupt.edu.cn>
2019-12-17 15:01:02 +0800

    disconf

--- application/src/java/com/tjlcast/server/SRApplication.java
+++ application/src/java/com/tjlcast/server/SRApplication.java
@@ -2,8 +2,10 @@ package com.tjlcast.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by tangjialiang on 2017/12/19.
@@ -11,8 +13,9 @@ import org.springframework.context.annotation.PropertySource;
  */
 @SpringBootApplication
 @PropertySource({"classpath:disconf.properties"})
-@ImportResource({"classpath:disconf.xml"})//引入disconf
+//@ImportResource({"classpath:disconf.xml"})//引入disconf
 public class SRApplication {
+    ApplicationContext context = new ClassPathXmlApplicationContext("disconf.xml");
     public static void main(String[] args) {
         SpringApplication.run(SRApplication.class, args);
     }
